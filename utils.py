2f88e300b7411b464c218be3432b3a250274cbfc
Aeron <ajlaffere@gmail.com>
2024-06-12 22:35:27 +0100

    refactor

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
-"""
-"""
+"""This module provides utilities for transcription."""
 
 import datetime
 import ffmpeg
@@ -46,71 +45,52 @@ def convert_to_timestamp(seconds):
     timestamp = "{:02}:{:02}:{:02}".format(int(hours), int(minutes), int(seconds))
     return timestamp
 
-def transcribe_audio_segments(audio_segments, filename, save=True):
-    transcribed_audio_segments = []
-    n_segments = len(audio_segments)
-    print(f"Transcribing {n_segments} audio segments. Estimated time: {n_segments * 10} seconds.")
-    for index, audio_segment in enumerate(audio_segments):
-        buffer = BytesIO()
-        buffer.name = "buffer.wav"
-        audio_segment.export(buffer, format="wav")
-        buffer.seek(0)
-        transcribed_audio_segment = whisper.transcribe_audio_segment(buffer)
-        if index == 0:
-            last_text_segment_id = transcribed_audio_segment[-1]['id']
-            pass
-        else:
-            for index, text_segment in enumerate(transcribed_audio_segment):
-                text_segment['id'] = last_text_segment_id + index + 1
-            last_text_segment_id = transcribed_audio_segment[-1]['id']
-        transcribed_audio_segments.append(transcribed_audio_segment)
+def buffer_audio_segment_and_transcribe(audio_segment, index):
+    buffer = BytesIO()
+    buffer.name = "buffer.wav"
+    audio_segment.export(buffer, format="wav")
+    buffer.seek(0)
+    transcribed_audio_segment = whisper.transcribe_audio_segment(buffer)
+    if index == 0:
+        last_text_segment_id = transcribed_audio_segment[-1]['id']
+        pass
+    else:
+        for index, text_segment in enumerate(transcribed_audio_segment):
+            text_segment['id'] = last_text_segment_id + index + 1
+        last_text_segment_id = transcribed_audio_segment[-1]['id']
+    return transcribed_audio_segment
 
-    for index, segment in enumerate(transcribed_audio_segments):
+def chunk_transcript_to_token_limit(transcript_segments, token_limit=1200):
+    for index, segment in enumerate(transcript_segments):
         if index == 0:
-            last_end_time = 0
-        else:
-            previous_index = index - 1
-            last_end_time = transcribed_audio_segments[previous_index][-1]['end']
-        for phrase_index, phrase in enumerate(segment):
-            transcribed_audio_segments[index][phrase_index]['start'] = phrase['start'] + last_end_time
-            transcribed_audio_segments[index][phrase_index]['end'] = phrase['end'] + last_end_time
+                text_buffer = ''
+                chunks = []
             
-    combined_transcript_segments = [item for sublist in transcribed_audio_segments for item in sublist]
-    
-    for index, segment in enumerate(combined_transcript_segments):
-        combined_transcript_segments[index]['start'] = convert_to_timestamp(segment['start'])
-        combined_transcript_segments[index]['end'] = convert_to_timestamp(segment['end'])
-        
-    if save:
-        with open(f'{filename}_whisper_output.json', 'w') as json_file:
-            json.dump(combined_transcript_segments, json_file)
-            
-    for index, segment in enumerate(combined_transcript_segments):
-        if index == 0:
-            text_buffer = ''
-            chunks = []
-        
         text_buffer += segment['text']
         token_count = len(encoding.encode(text_buffer))
         
-        if index == len(combined_transcript_segments) - 1:
+        if index == len(transcript_segments) - 1:
             chunks.append(text_buffer)
             text_buffer = ''
             break
         
-        if token_count > 1200:
+        if token_count > token_limit:
             if text_buffer.strip().endswith('.'):
                 chunks.append(text_buffer)
                 text_buffer = ''
-    
+                
+        return chunks, len(chunks)
+        
+def load_system_prompt():
     module_path = os.path.abspath(__file__)
     prompt_path = os.path.join(os.path.dirname(module_path), "data", "prompt.txt")
     
     with open(prompt_path, "r") as f:
         system_prompt = f.read()
-    
-    n_transcript_chunks = len(chunks)
-    print(f"Processing {n_transcript_chunks} transcript chunks. Estimated time: {n_transcript_chunks * 30} seconds.")            
+    return system_prompt
+
+def process_transcription(chunks):
+    system_prompt = load_system_prompt()
     processed_chunks = []
     for index, chunk in enumerate(chunks):
         if index == 0:
@@ -134,7 +114,42 @@ def transcribe_audio_segments(audio_segments, filename, save=True):
         chunk_items = list(processed_chunk.items())
         
     combined_processed_chunks =  {key: value for d in processed_chunks for key, value in d.items()}
+    return combined_processed_chunks
 
+def transcribe_audio_segments(audio_segments, filename, save=True):
+    n_segments = len(audio_segments)
+    print(f"Transcribing {n_segments} audio segments. Estimated time: {n_segments * 10} seconds.")
+    
+    transcribed_audio_segments = []
+    for index, audio_segment in enumerate(audio_segments):
+        transcribed_audio_segment = buffer_audio_segment_and_transcribe(audio_segment, index)
+        transcribed_audio_segments.append(transcribed_audio_segment)
+
+    for index, segment in enumerate(transcribed_audio_segments):
+        if index == 0:
+            last_end_time = 0
+        else:
+            previous_index = index - 1
+            last_end_time = transcribed_audio_segments[previous_index][-1]['end']
+        for phrase_index, phrase in enumerate(segment):
+            transcribed_audio_segments[index][phrase_index]['start'] = phrase['start'] + last_end_time
+            transcribed_audio_segments[index][phrase_index]['end'] = phrase['end'] + last_end_time
+            
+    combined_transcript_segments = [item for sublist in transcribed_audio_segments for item in sublist]
+    
+    for index, segment in enumerate(combined_transcript_segments):
+        combined_transcript_segments[index]['start'] = convert_to_timestamp(segment['start'])
+        combined_transcript_segments[index]['end'] = convert_to_timestamp(segment['end'])
+        
+    if save:
+        with open(f'{filename}_whisper_output.json', 'w') as json_file:
+            json.dump(combined_transcript_segments, json_file)
+            
+    chunks, n_transcript_chunks = chunk_transcript_to_token_limit(combined_transcript_segments, token_limit=1200)    
+    
+    print(f"Processing {n_transcript_chunks} transcript chunks. Estimated time: {n_transcript_chunks * 30} seconds.")  
+    
+    combined_processed_chunks = process_transcription(chunks)
     if save:
         with open(f'{filename}_final_output.json', 'w') as json_file:
             json.dump(combined_processed_chunks, json_file)
