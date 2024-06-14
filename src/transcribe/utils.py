ecf23c7cbef8f78bc77b8a313b7a1a10c15624fc
Aeron <ajlaffere@gmail.com>
2024-06-14 20:12:45 +0100

    some type hints, fix to gpt call for linting

--- src/transcribe/utils.py
+++ src/transcribe/utils.py
@@ -10,15 +10,16 @@ from pydub import AudioSegment
 import tiktoken
 from . import whisper, gpt
 from .openai import OpenAIClient
+from typing import Tuple
 
 encoding = tiktoken.get_encoding("cl100k_base")
 
-def split_filepath(filepath):
+def split_filepath(filepath: str) -> Tuple[str, str]:
     filename, extension = os.path.splitext(filepath)
     print(f"Filename: {filename}, Extension: {extension}")
     return filename, extension  
 
-def convert_input_to_wav(filename, filetype):
+def convert_input_to_wav(filename: str, filetype: str) -> None:
     try: 
         (
             ffmpeg
@@ -32,10 +33,14 @@ def convert_input_to_wav(filename, filetype):
         print('stderr:', e.stderr.decode('utf8'))
     return None
     
-def segment_audio(audio_file, segment_duration):
+def segment_audio(wav_filepath: str, segment_duration: int) -> list[AudioSegment]:
+    """
+    Segments an audio file into smaller chunks.
+    """
     def check_segment_end_time(proposed_end_time, max_total_duration):
         return proposed_end_time if proposed_end_time < max_total_duration else max_total_duration
-    audio_to_segment = AudioSegment.from_file(audio_file, format="wav")
+    
+    audio_to_segment = AudioSegment.from_file(wav_filepath, format="wav")
     audio_duration = len(audio_to_segment)
     n_segments = int(np.ceil(audio_duration / segment_duration))
     audio_segments = []
@@ -48,6 +53,27 @@ def segment_audio(audio_file, segment_duration):
         audio_segments.append(audio_to_segment[start_time:end_time])
     
     return audio_segments
+
+def save_whisper_output(combined_transcript_segments, filename):
+    """
+    
+    """
+    with open(f'{filename}_whisper_output.json', 'w') as json_file:
+        json.dump(combined_transcript_segments, json_file)
+    return None
+
+def load_whisper_output(filepath):
+    with open(f"{filepath}_whisper_output.json", "r") as f:
+        whisper_output = json.load(f)
+    return whisper_output
+
+def save_final_output(combined_processed_chunks, filename):
+    """
+    
+    """
+    with open(f'{filename}_final_output.json', 'w') as json_file:
+        json.dump(combined_processed_chunks, json_file)
+    return None
 
 def convert_to_timestamp(seconds):
     td = datetime.timedelta(seconds=seconds)
@@ -73,11 +99,9 @@ def renumber_transcribed_audio_segment(transcribed_audio_segment, number_to_star
     return transcribed_audio_segment
 
 def chunk_transcript_to_token_limit(transcript_segments, token_limit=1200):
-    for index, segment in enumerate(transcript_segments):
-        if index == 0:
-                text_buffer = ''
-                chunks = []
-            
+    text_buffer = ''
+    chunks = []
+    for index, segment in enumerate(transcript_segments):    
         text_buffer += segment['text']
         token_count = len(encoding.encode(text_buffer))
         
@@ -90,8 +114,8 @@ def chunk_transcript_to_token_limit(transcript_segments, token_limit=1200):
             if text_buffer.strip().endswith('.'):
                 chunks.append(text_buffer)
                 text_buffer = ''
-                
-        return chunks, len(chunks)
+                    
+    return chunks, len(chunks)
         
 def load_system_prompt():
     module_path = os.path.abspath(__file__)
@@ -104,11 +128,19 @@ def load_system_prompt():
 def process_transcription(chunks):
     system_prompt = load_system_prompt()
     processed_chunks = []
+    
+    prompt = system_prompt
     for index, chunk in enumerate(chunks):
-        if index == 0:
-            completion = gpt.process_transcription(chunk, system_prompt)
-        else:
-            annotated_system_prompt = system_prompt + f"""
+        completion = gpt.process_transcription(chunk, prompt)
+        processed_chunk = json.loads(completion.choices[0].message.content)
+        processed_chunks.append(processed_chunk)
+        chunk_items = list(processed_chunk.items())
+        
+        print(f"Processed chunk at index {index}")
+        print(f"Finish reason: {completion.choices[0].finish_reason}")
+        
+        if index > 0:
+            prompt = system_prompt + f"""
             ---
             You are continuing on from a previous transcription which ended as follows:
 
@@ -117,13 +149,7 @@ def process_transcription(chunks):
             "{chunk_items[-2][0]}" : {chunk_items[-2][1]},
             "{chunk_items[-1][0]}" : {chunk_items[-1][1]}
             """
-            completion = gpt.process_transcription(chunk, annotated_system_prompt)
-        print(f"Processed chunk at index {index}")
-        print(f"Finish reason: {completion.choices[0].finish_reason}")
-        
-        processed_chunk = json.loads(completion.choices[0].message.content)
-        processed_chunks.append(processed_chunk)
-        chunk_items = list(processed_chunk.items())
-        
+    
+    # Combine the processed chunks        
     combined_processed_chunks =  {key: value for d in processed_chunks for key, value in d.items()}
     return combined_processed_chunks
