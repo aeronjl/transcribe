db118daf81b69d7f24b3ba45f70628470feb94b7
Aeron <ajlaffere@gmail.com>
2024-06-21 19:14:44 +0100

    rearrange functions

--- src/precisetranscribe/__init__.py
+++ src/precisetranscribe/__init__.py
@@ -1,38 +1,11 @@
 from . import utils, whisper
 
-def transcribe_audio(wav_data, save=False, filename=None):
+def transcribe_audio(wav_data):
     """
-    Transcribes audio data from a WAV file in segmented chunks, processes each segment,
-    and saves the results as JSON files.
-
-    Args:
-    - wav_data (bytes): Audio data in WAV format.
-    - save (bool, optional): Whether to save output JSON files (default: True).
-
-    Returns:
-    - tuple: A tuple containing two lists:
-        - combined_transcript_segments (list): List of all transcribed segments with start/end times.
-        - combined_processed_chunks (list): List of all processed transcript chunks.
-
-    Notes:
-    - Uses utility functions from 'utils' for segmenting audio, buffering, renumbering segments,
-      converting times to timestamps, chunking transcripts, and processing with GPT-4o.
-    - Requires 'whisper' for actual transcription and 'utils' for various utility functions.
-
-    Example usage:
-    >>> audio_data = load_wav_file('audio.wav')
-    >>> transcriptions, processed_chunks = transcribe_audio(audio_data)
     """
-    
-    # Segment the audio file into smaller chunks for transcribing
     audio_segments = utils.segment_audio(wav_data, 100000)
     n_segments = len(audio_segments)
-    
-    # os.system('cls' if os.name == 'nt' else 'clear')
-    
     print(f"Transcribing {n_segments} audio segments. Estimated time: {n_segments * 10} seconds.")
-    
-    # Transcribe the audio segments
     transcribed_audio_segments = []
     for index, audio_segment in enumerate(audio_segments):
         print(f"Transcribing audio segment {index + 1}/{n_segments}")
@@ -54,17 +27,33 @@ def transcribe_audio(wav_data, save=False, filename=None):
     for index, segment in enumerate(combined_transcript_segments):
         combined_transcript_segments[index]['start'] = utils.convert_to_timestamp(segment['start'])
         combined_transcript_segments[index]['end'] = utils.convert_to_timestamp(segment['end'])  
-    
-    # Chunk the transcript segments to a token limit
-    chunks, n_transcript_chunks = utils.chunk_transcript_to_token_limit(combined_transcript_segments, token_limit=1200)    
+        
+    return combined_transcript_segments
 
-    # os.system('cls' if os.name == 'nt' else 'clear')
+def process_whisper_transcription(transcribed_audio_segments):
+        """
+        """
+        # Rearrange the transcript segments into chunks which fit the token limit
+        chunks, n_transcript_chunks = utils.chunk_transcript_to_token_limit(transcribed_audio_segments, token_limit=1200)    
+
+        print(f"Processing {n_transcript_chunks} transcript chunks. Estimated time: {n_transcript_chunks * 30} seconds.")  
+        processed_transcript = utils.process_transcription(chunks)
+        # os.system('cls' if os.name == 'nt' else 'clear')
+        return processed_transcript
     
-    # Process the transcription chunks with GPT-4o
-    print(f"Processing {n_transcript_chunks} transcript chunks. Estimated time: {n_transcript_chunks * 30} seconds.")  
-    combined_processed_chunks = utils.process_transcription(chunks)
+def run(wav_data, save=False, filename=None):
+    """
+    """
+    
+    whisper_output = transcribe_audio(wav_data)
+    
+    if save:
+        utils.save_whisper_output(whisper_output, filename)
+    
+    # 3. Process the Whisper transcription
+    processed_transcript = process_whisper_transcription(whisper_output)
 
     if save:
-        utils.save_final_output(combined_processed_chunks, filename)
+        utils.save_final_output(processed_transcript, filename)
         
-    return combined_transcript_segments, combined_processed_chunks
+    return processed_transcript
