feb07efa60ffd9194c277b298bf4209349248529
Aeron <ajlaffere@gmail.com>
2024-06-22 02:34:48 +0100

    remove elapsed time (obviated)

--- src/precisetranscribe/whisper.py
+++ src/precisetranscribe/whisper.py
@@ -1,5 +1,3 @@
-import time
-
 from . import utils
 from .openai import OpenAIClient
 
@@ -10,18 +8,12 @@ def transcribe_audio_segment(audio_segment):
     """
     Call Whisper transcription for a short sample of audio.
     """
-    
     buffer = utils.buffer_audio(audio_segment) 
-    
-    t = time.time()
     transcription = openai.audio.transcriptions.create(
         model="whisper-1",
         file=buffer,
         language="en",
         response_format="verbose_json",
         temperature=0.2
-    )
-    elapsed = time.time() - t
-    print(f"Transcribed audio segment. Elapsed: {elapsed} seconds.")
-    
+    )    
     return transcription.segments
