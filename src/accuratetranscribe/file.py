cae5987c041b43b3e704cf5300c1d348f4741a2a
Aeron <ajlaffere@gmail.com>
2024-06-22 07:17:32 +0100

    file saving for aligned transcripts

--- src/accuratetranscribe/file.py
+++ src/accuratetranscribe/file.py
@@ -1,11 +1,11 @@
 import json
 from typing import List, Dict, Any
 
-def save_whisper_output(combined_transcript_segments: List[Dict[str, Any]], filename: str) -> None:
+def save_whisper_output(combined_transcript_segments: List[Dict[str, Any]], filepath: str) -> None:
     """
     
     """
-    with open(f'{filename}_whisper_output.json', 'w') as json_file:
+    with open(f'{filepath}_whisper_output.json', 'w') as json_file:
         json.dump(combined_transcript_segments, json_file, indent=4)
     return None
 
@@ -14,11 +14,16 @@ def load_whisper_output(filepath: str) -> List[Dict[str, Any]]:
         whisper_output = json.load(f)
     return whisper_output
 
-def save_processed_transcript(combined_processed_chunks: List[Dict[str, Any]], filename: str) -> None:
+def save_processed_transcript(combined_processed_chunks: List[Dict[str, Any]], filepath: str) -> None:
     """
     """
-    with open(f'{filename}_final_output.json', 'w') as json_file:
+    with open(f'{filepath}_final_output.json', 'w') as json_file:
         json.dump(combined_processed_chunks, json_file, indent=4)
+    return None
+
+def save_aligned_transcript(aligned_transcript, filepath) -> None:
+    with open(f'{filepath}_aligned_transcript.json', 'w') as f:
+        json.dump(aligned_transcript, f, indent=2)
     return None
 
 # Main execution
