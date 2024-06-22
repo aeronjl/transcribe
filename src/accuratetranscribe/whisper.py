598cc2ddb7d4a6d57416dd9fa7e3a7a44733d8b8
Aeron <ajlaffere@gmail.com>
2024-06-22 20:26:17 +0100

    prompt and temperature change

--- src/accuratetranscribe/whisper.py
+++ src/accuratetranscribe/whisper.py
@@ -14,8 +14,6 @@ def transcribe_audio_segment(audio_segment):
         file=buffer,
         language="en",
         response_format="verbose_json",
-        temperature=0,
-        prompt="The audio provided may have moments of silence, do not make up words to fill in extended times of silence."
-        
+        temperature=0.1,
     )    
     return transcription.segments
