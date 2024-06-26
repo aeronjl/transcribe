eca64178a2b14a2afaed7fb410e3d381514b556a
Aeron <ajlaffere@gmail.com>
2024-06-26 17:12:44 +0100

    explicit typing

--- src/accuratetranscribe/whisper.py
+++ src/accuratetranscribe/whisper.py
@@ -1,21 +1,24 @@
+from pydub import AudioSegment
+
 from . import utils
 from .openai import OpenAIClient
+from .datastructures import WhisperOutput
 
 client = OpenAIClient()
 openai = client.get_openai()
 
 
-def transcribe_audio_segment(audio_segment):
+def transcribe_audio_segment(audio_segment: AudioSegment) -> WhisperOutput:
     """
     Call Whisper transcription for a short sample of audio.
     """
     buffer = utils.buffer_audio(audio_segment)
-    transcription = openai.audio.transcriptions.create(
+    transcription: WhisperOutput = openai.audio.transcriptions.create(
         model="whisper-1",
         file=buffer,
         language="en",
         response_format="verbose_json",
         temperature=0.1,
-    )
-    return transcription.segments
+    ).segments
+    return transcription
 
